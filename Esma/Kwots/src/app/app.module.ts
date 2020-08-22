63261a68c9a089ad02cfcdc5b11b03440c0ba827
virsail <virsaileric@gmail.com>
2020-08-22 20:36:09 +0300

    add the code for my form input

--- Esma/Kwots/src/app/app.module.ts
+++ Esma/Kwots/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
+    FormComponent,
     
   ],
   imports: [
