c7ecf04bed5df414cde94f135663b41b57dc5c1d
virsail <virsaileric@gmail.com>
2020-08-22 17:44:21 +0300

    add code to convert current time

--- Esma/Kwots/src/app/app.module.ts
+++ Esma/Kwots/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { TimeComponent } from './time/time.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    TimeComponent
   ],
   imports: [
     BrowserModule,
