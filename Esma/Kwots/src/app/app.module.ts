fb17693d518b6320a0df21405603b5e5561d6678
virsail <virsaileric@gmail.com>
2020-08-22 18:10:29 +0300

    add the text in the timer section

--- Esma/Kwots/src/app/app.module.ts
+++ Esma/Kwots/src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { TimeComponent } from './time/time.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    TimeComponent
+    
   ],
   imports: [
     BrowserModule,
