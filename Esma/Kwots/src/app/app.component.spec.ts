cf020f37aefa911b557043665e213c8b3b1b5c15
virsail <virsaileric@gmail.com>
2020-08-23 22:03:39 +0300

    save the changes

--- Esma/Kwots/src/app/app.component.spec.ts
+++ Esma/Kwots/src/app/app.component.spec.ts
@@ -2,6 +2,8 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,6 +20,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+
   });
 
   it(`should have as title 'Kwots'`, () => {
@@ -29,7 +32,7 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Kwots app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Kwots-app!');
   });
 });
