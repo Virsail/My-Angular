8b918ee9ed41858d30f2a0463fe8b696594d1527
virsail <virsaileric@gmail.com>
2020-08-23 16:36:32 +0300

    import the class on quotes.ts

--- Esma/Kwots/src/app/quote.ts
+++ Esma/Kwots/src/app/quote.ts
@@ -1,4 +1,9 @@
+import { ɵBrowserPlatformLocation } from '@angular/platform-browser';
+
 export class Quote {
-    id: number;
-    name: string;
+  showInfo: boolean;
+  constructor(public id:number,public name:string,public title:string,public quote:string,public author:string, public datePosted: Date,public likes:number, public dislikes:number){
+      this.showInfo=false;
+    }
 }
+
